373128171a44a69ca6c7101c1b6920a4c10bd2d3
Gorilla99-max <nmriza3@gmail.com>
2024-07-14 16:10:29 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7426916010:AAFPUiGevaq6Oz6wmfZApzj7Gx28-sYy4-E'
+ADMIN_ID = '6039703925'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
